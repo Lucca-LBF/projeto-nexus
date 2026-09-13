df8eb38d37e7b219f074aa9ec232a6a82a0dc473
Lucca <lucca4theo@gmail.com>
2026-09-13 18:29:46 -0300

    path fixed

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('simulador/', include('simulador.urls')),
+    path('', RedirectView.as_view(url='/simulador/resultado/', permanent=False)),
 ]
