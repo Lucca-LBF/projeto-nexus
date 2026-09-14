668134947671a960f09690affda7562fdcda3345
Lucca <lucca4theo@gmail.com>
2026-09-14 10:53:40 -0300

    added contact form

--- config/settings.py
+++ config/settings.py
@@ -109,8 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
-
-TIME_ZONE = 'sao_paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
