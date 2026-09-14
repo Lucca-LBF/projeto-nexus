e3259d31322b3b67875548170f0b89d157b8fcf8
Lucca <lucca4theo@gmail.com>
2026-09-14 10:08:32 -0300

    added contact form

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'), 
     path('simulador/', include('simulador.urls')), 
     #path('dashboard/', include('dashboard.urls')),
+    path('contato/', views.contato, name='contato'),
 ]
--- config/views.py
+++ config/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from simulador.models import MensagemContato 
 
 def home(request):
     return render(request, 'home.html')
+
+def contato(request):
+    sucesso = False 
+    
+    if request.method == 'POST':
+        nome_digitado = request.POST.get('nome')
+        email_digitado = request.POST.get('email')
+        assunto_digitado = request.POST.get('assunto')
+        mensagem_digitada = request.POST.get('mensagem')
+
+        MensagemContato.objects.create(
+            nome=nome_digitado,
+            email=email_digitado,
+            assunto=assunto_digitado,
+            mensagem=mensagem_digitada
+        )
+        sucesso = True
+    return render(request, 'contato.html', {'sucesso': sucesso})
